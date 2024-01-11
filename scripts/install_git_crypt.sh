1086b3fe8aaf077bf19158cb5578194b8ab3ad78
David Young <davidy@funkypenguin.co.nz>
2024-01-11 22:19:08 +1300

    Bump to Alpine 3.19.0, git-crypt v0.7.0
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/install_git_crypt.sh
+++ scripts/install_git_crypt.sh
@@ -3,15 +3,16 @@
 set -eu
 
 _main() {
-  apk --update add --virtual=.build-deps curl make g++ openssl-dev
+  apk --update add --virtual=.build-deps curl make g++ openssl-dev git
   apk add --no-cache libgcc libstdc++ openssl
   local tmpdir
   tmpdir="$(mktemp -d git_crypt_install.XXXXXX)"
 
   cd "$tmpdir"
-  curl -Lo git-crypt-0.6.0.tar.gz https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.6.0.tar.gz
-  tar -zxf git-crypt-0.6.0.tar.gz
-  cd git-crypt-0.6.0
+  curl -Lo git-crypt-0.7.0.tar.gz https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.7.0.tar.gz
+  tar -zxf git-crypt-0.7.0.tar.gz
+  cd git-crypt-0.7.0
+  curl -L https://patch-diff.githubusercontent.com/raw/AGWA/git-crypt/pull/249.patch | git apply -v
   make
   make install
   cd ..
